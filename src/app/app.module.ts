e2730217f0aec5bd2028a59e86382587eb4f665c
KosmosLock <cos.lo.iac@gmail.com>
2021-10-30 18:54:25 +0200

    FirstCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { OrderContainerComponent } from './order-container/order-container.component';
+import { UserSingleComponent } from './user-single/user-single.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import { OrderSingleComponent } from './order-single/order-single.component';
+import { OrderDetailComponent } from './order-detail/order-detail.component';
+import { UserContainerComponent } from './user-container/user-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    OrderContainerComponent,
+    UserSingleComponent,
+    UserDetailComponent,
+    OrderListComponent,
+    OrderSingleComponent,
+    OrderDetailComponent,
+    UserContainerComponent
   ],
   imports: [
     BrowserModule
